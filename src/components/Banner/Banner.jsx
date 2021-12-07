f119320a54f8f3265fcb9f9c2aef35dabd8a6486
Tyrel Chambers <tychambers3@gmail.com>
2021-12-06 20:28:04 -0500

    add banner framework

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledWrapper = styled.section``;
+
+const Banner = () => {
+  return <StyledWrapper></StyledWrapper>;
+};
+
+export default Banner;
