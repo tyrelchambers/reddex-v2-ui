cff8400974c36dff4bf0edd6fc70b00df2eab785
Tyrel Chambers <tychambers3@gmail.com>
2022-01-11 18:51:48 -0500

    add production config

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,13 @@ const config = {
     redditRedirect: "http://localhost:3000/callback/reddit",
     youtubeKey: "AIzaSyBzE5hSoh6JZsLsepRXULe9yx2o-KcoJrs",
   },
+  production: {
+    backend: "https://reddex-staging.com",
+    frontend: "https://reddex-staging.com",
+    redditApp: "Rw_vFfmZPN4uYA",
+    redditRedirect: "https://reddex-staging.com/callback/reddit",
+    youtubeKey: "AIzaSyBzE5hSoh6JZsLsepRXULe9yx2o-KcoJrs",
+  },
 };
 
 export default config;
