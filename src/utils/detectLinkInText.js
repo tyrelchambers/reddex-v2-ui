4e9cc39bc458c0c7f6fc43319b40d0902cf57172
Tyrel Chambers <tychambers3@gmail.com>
2022-01-28 22:34:46 -0500

    fix detect text bug

--- src/utils/detectLinkInText.js
+++ src/utils/detectLinkInText.js
@@ -1,17 +1,22 @@
 export const detectLinkInText = (str) => {
+  if (!str) return "";
+
   let match = str.match(
     /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi
   );
   let final = str;
-  match.map((url) => {
-    final = final.replace(
-      url,
-      '<a href="' +
-        url +
-        '" target="_BLANK" class="text-accent-primary">' +
-        url +
-        "</a>"
-    );
-  });
+
+  if (match) {
+    match.map((url) => {
+      final = final.replace(
+        url,
+        '<a href="' +
+          url +
+          '" target="_BLANK" class="text-accent-primary">' +
+          url +
+          "</a>"
+      );
+    });
+  }
   return final;
 };
